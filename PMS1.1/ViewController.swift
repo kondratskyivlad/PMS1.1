e0558b7754ffc5ea3c4b26c933471004b4833fdd
kondratskyivlad <55400806+kondratskyivlad@users.noreply.github.com>
2021-02-17 15:35:28 +0200

    Initial Commit

---
+++ PMS1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PMS1.1
+//
+//  Created by Влад Кондрацький on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PMS1.1Tests/PMS1_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  PMS1_1Tests.swift
+//  PMS1.1Tests
+//
+//  Created by Влад Кондрацький on 17.02.2021.
+//
+
+import XCTest
+@testable import PMS1_1
+
+class PMS1_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
